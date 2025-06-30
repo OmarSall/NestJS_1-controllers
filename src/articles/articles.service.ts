50327e1de7b576d4a3176993967768b2c836d3ee
Omar Salloum <omarsalloum08@gmail.com>
2025-06-30 18:50:23 +0200

    small fix

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,77 +1,72 @@
-import {Injectable, NotFoundException} from "@nestjs/common";
-import {Article} from "./article"
-import {ArticleDto} from "./articles.dto"
-import {LoggerService} from "../logger/logger.service";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Article } from './article';
+import { ArticleDto } from './articles.dto';
+import { LoggerService } from '../logger/logger.service';
 
 @Injectable()
 export class ArticlesService {
-    constructor(private readonly loggerService: LoggerService) {
+  constructor(private readonly loggerService: LoggerService) {}
 
+  private articles: Article[] = [];
+  private nextCreatedArticleId: number = 1;
+
+  getAll() {
+    this.loggerService.log('Getting a list of all articles');
+    return this.articles;
+  }
+
+  getById(id: number) {
+    this.loggerService.log('Getting a list of all articles');
+    const article = this.articles.find((article) => article.id === id);
+
+    if (article) {
+      return article;
     }
 
-    private articles: Article[] = [];
-    private nextCreatedArticleId: number = 1;
+    this.loggerService.warn('Trying to access an article that does not exist');
+    throw new NotFoundException(`Article with id ${id} not found`);
+  }
 
+  create(article: ArticleDto) {
+    this.loggerService.log('Creating new article');
+    const newArticle = {
+      id: this.nextCreatedArticleId++,
+      ...article,
+    };
+    this.articles.push(newArticle);
+    return newArticle;
+  }
 
-    getAll() {
-        this.loggerService.log("Getting a list of all articles");
-        return this.articles;
+  update(id: number, article: ArticleDto) {
+    this.loggerService.log('Updating an article');
+    const articleIndex = this.articles.findIndex(
+      (article) => article.id === id,
+    );
+    if (articleIndex === -1) {
+      this.loggerService.warn(
+        'Trying to update an article that does not exist',
+      );
+      throw new NotFoundException(`Article with id ${id} not found`);
     }
+    this.articles[articleIndex] = {
+      ...this.articles[articleIndex],
+      ...article
+    };
 
-    getById(id: number) {
-        this.loggerService.log("Getting a list of all articles");
-        const article = this.articles.find(article => article.id === id)
+    return this.articles[articleIndex];
+  }
 
-        if (article) {
-            return article;
-        }
-
-        this.loggerService.warn("Trying to access an article that does not exist");
-        throw new NotFoundException(`Article with id ${id} not found`);
-
+  delete(id: number) {
+    this.loggerService.log('Deleting an article');
+    const articleIndex = this.articles.findIndex(
+      (article) => article.id === id,
+    );
+    if (articleIndex === -1) {
+      this.loggerService.warn(
+        'Trying to delete an article that does not exist',
+      );
+      throw new NotFoundException(`Article with id ${id} not found`);
     }
-
-    create(article: ArticleDto) {
-        this.loggerService.log("Creating new article");
-        const newArticle = {
-            id: this.nextCreatedArticleId++,
-            ...article,
-        }
-        this.articles.push(newArticle);
-        return newArticle;
-    }
-
-    update(id: number, article: ArticleDto) {
-        this.loggerService.log("Updating an article");
-        const articleIndex = this.articles.findIndex(
-            (article) => article.id === id
-        );
-        if (articleIndex === -1) {
-            this.loggerService.warn(
-                "Trying to update an article that does not exist"
-            );
-            throw new NotFoundException(`Article with id ${id} not found`);
-        }
-        this.articles[articleIndex] = {
-            ...this.articles[articleIndex],
-            title: article.title,
-            content: article.content,
-        }
-
-        return this.articles[articleIndex];
-    }
-
-    delete(id: number) {
-        this.loggerService.log("Deleting an article");
-        const articleIndex = this.articles.findIndex(
-            (article) => article.id === id,
-        );
-        if (articleIndex === -1) {
-            this.loggerService.warn(
-                "Trying to delete an article that does not exist"
-            );
-            throw new NotFoundException(`Article with id ${id} not found`);
-        }
-        this.articles.splice(articleIndex, 1);
-    }
+    this.articles.splice(articleIndex, 1);
+  }
 }
